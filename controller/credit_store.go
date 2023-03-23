6e69bd8f4a50d98fb47a17d1e5b8d1c91654f9f5
gas <gas@pop-os.localdomain>
2023-03-24 01:34:40 +0700

    wallet -payment

--- controller/credit_store.go
+++ controller/credit_store.go
@@ -6,6 +6,7 @@ import (
 	"gsc/utils"
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
@@ -24,6 +25,36 @@ func CreditStore(db *gorm.DB, q *gin.Engine) {
 		}
 
 		utils.HttpRespSuccess(c, http.StatusOK, "All credit store", scores)
+	})
+
+	// view cart
+	r.GET("/view-cart", middleware.Authorization(), func(c *gin.Context) {
+		var total int
+		var totalPoints int
+
+		ID, _ := c.Get("id")
+		userID, ok := ID.(uuid.UUID)
+		if !ok {
+			utils.HttpRespFailed(c, http.StatusNotFound, "User not found")
+			return
+		}
+
+		var cart []model.CreditStoreWallet
+		if err := db.Where("user_id = ?", userID).Find(&cart).Error; err != nil {
+			utils.HttpRespFailed(c, http.StatusNotFound, err.Error())
+			return
+		}
+
+		for _, v := range cart {
+			total += v.Price
+			totalPoints += v.Points
+		}
+
+		utils.HttpRespSuccess(c, http.StatusOK, "View cart", gin.H{
+			"total":       total,
+			"totalPoints": totalPoints,
+			"cart":        cart,
+		})
 	})
 
 	// add to cart
@@ -47,10 +78,36 @@ func CreditStore(db *gorm.DB, q *gin.Engine) {
 			return
 		}
 
+		parsedID, err := strconv.ParseUint(strconv.Itoa(input.ID), 10, 0)
+		if err != nil {
+			utils.HttpRespFailed(c, http.StatusUnprocessableEntity, err.Error())
+			return
+		}
+
 		addToCart := model.CreditStoreWallet{
-			UserID: userID,
-			Points: credit.Points,
-			Price:  credit.Price,
+			UserID:        userID,
+			CreditStoreID: uint(parsedID),
+			Points:        credit.Points,
+			Price:         credit.Price,
+			Quantity:      1,
+		}
+
+		// handle error if user already add to cart
+		var isExist model.CreditStoreWallet
+		if err := db.Where("user_id = ? ", userID).Where("credit_store_id = ?", input.ID).First(&isExist).Error; err == nil {
+			log.Println("sudah ada di cart")
+			// update
+			isExist.Points += credit.Points
+			isExist.Price += credit.Price
+			isExist.Quantity += 1
+			if err := db.Where("user_id = ?", userID).Where("credit_store_id = ?", input.ID).Save(&isExist).Error; err != nil {
+				log.Println("error update")
+				utils.HttpRespFailed(c, http.StatusBadGateway, err.Error())
+				return
+			}
+
+			utils.HttpRespSuccess(c, http.StatusOK, "added to cart", isExist)
+			return
 		}
 
 		if err := db.Create(&addToCart).Error; err != nil {
@@ -59,6 +116,117 @@ func CreditStore(db *gorm.DB, q *gin.Engine) {
 		}
 
 		utils.HttpRespSuccess(c, http.StatusOK, "Add to cart", addToCart)
+	})
+
+	// add 1 amount by id
+	r.POST("/add-amount/:itemID", middleware.Authorization(), func(c *gin.Context) {
+		itemID := c.Param("itemID")
+
+		var credit model.CreditStore
+		if err := db.Where("id = ?", itemID).First(&credit).Error; err != nil {
+			utils.HttpRespFailed(c, http.StatusNotFound, err.Error())
+			return
+		}
+
+		ID, _ := c.Get("id")
+		userID, ok := ID.(uuid.UUID)
+		if !ok {
+			utils.HttpRespFailed(c, http.StatusNotFound, "User not found")
+			return
+		}
+
+		var updated model.CreditStoreWallet
+		if err := db.Where("credit_store_id = ?", itemID).Where("user_id = ?", userID).First(&updated).Error; err != nil {
+			// utils.HttpRespFailed(c, http.StatusNotFound, err.Error())
+			// return
+
+			// its not in cart yet
+			addToCart := model.CreditStoreWallet{
+				UserID:        userID,
+				CreditStoreID: credit.ID,
+				Points:        credit.Points,
+				Price:         credit.Price,
+				Quantity:      1,
+			}
+
+			if err := db.Create(&addToCart).Error; err != nil {
+				utils.HttpRespFailed(c, http.StatusNotFound, err.Error())
+				return
+			}
+
+			utils.HttpRespSuccess(c, http.StatusOK, "added new item", addToCart)
+			return
+		}
+
+		updated.Points += credit.Points
+		updated.Price += credit.Price
+		updated.Quantity += 1
+
+		// check if exist
+		var isExist model.CreditStoreWallet
+		if err := db.Where("user_id = ?", userID).Where("credit_store_id = ?", credit.ID).First(&isExist).Error; err != nil {
+			utils.HttpRespFailed(c, http.StatusBadRequest, err.Error())
+			return
+		}
+
+		if err := db.Where("user_id = ?", userID).Where("credit_store_id = ?", credit.ID).Save(&updated).Error; err != nil {
+			utils.HttpRespFailed(c, http.StatusNotFound, err.Error())
+			return
+		}
+
+		utils.HttpRespSuccess(c, http.StatusOK, "Added 1 amount", updated)
+	})
+
+	// remove 1 amount by id
+	r.POST("/remove-amount/:itemID", middleware.Authorization(), func(c *gin.Context) {
+		itemID := c.Param("itemID")
+
+		var credit model.CreditStore
+		if err := db.Where("id = ?", itemID).First(&credit).Error; err != nil {
+			utils.HttpRespFailed(c, http.StatusNotFound, err.Error())
+			return
+		}
+
+		ID, _ := c.Get("id")
+		userID, ok := ID.(uuid.UUID)
+		if !ok {
+			utils.HttpRespFailed(c, http.StatusNotFound, "User not found")
+			return
+		}
+
+		var updated model.CreditStoreWallet
+		if err := db.Where("credit_store_id = ?", itemID).Where("user_id = ?", userID).First(&updated).Error; err != nil {
+			utils.HttpRespFailed(c, http.StatusNotFound, err.Error())
+			return
+		}
+
+		updated.Points -= credit.Points
+		updated.Price -= credit.Price
+		updated.Quantity -= 1
+
+		// check if exist
+		var isExist model.CreditStoreWallet
+		if err := db.Where("user_id = ?", userID).Where("credit_store_id = ?", credit.ID).First(&isExist).Error; err != nil {
+			utils.HttpRespFailed(c, http.StatusBadRequest, err.Error())
+			return
+		}
+
+		if updated.Quantity == 0 {
+			if err := db.Where("user_id = ?", userID).Where("credit_store_id = ?", credit.ID).Delete(&updated).Error; err != nil {
+				utils.HttpRespFailed(c, http.StatusNotFound, err.Error())
+				return
+			}
+
+			utils.HttpRespSuccess(c, http.StatusOK, "Removed all", updated)
+			return
+		}
+
+		if err := db.Where("user_id = ?", userID).Where("credit_store_id = ?", credit.ID).Save(&updated).Error; err != nil {
+			utils.HttpRespFailed(c, http.StatusNotFound, err.Error())
+			return
+		}
+
+		utils.HttpRespSuccess(c, http.StatusOK, "Removed 1 amount", updated)
 	})
 
 	// remove item from cart
--- model/credit_store_wallet.go
+++ model/credit_store_wallet.go
@@ -3,10 +3,13 @@ package model
 import "github.com/google/uuid"
 
 type CreditStoreWallet struct {
-	User   User      `gorm:"ForeignKey:UserID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
-	UserID uuid.UUID `json:"user_id"`
-	Points int       `json:"points"`
-	Price  int       `json:"price"`
+	User          User        `gorm:"ForeignKey:UserID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+	UserID        uuid.UUID   `json:"user_id"`
+	CreditStore   CreditStore `gorm:"ForeignKey:CreditStoreID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+	CreditStoreID uint        `json:"credit_store_id"`
+	Points        int         `json:"points"`
+	Price         int         `json:"price"`
+	Quantity      int         `json:"quantity"`
 }
 
 type CreditStoreWalletInput struct {
